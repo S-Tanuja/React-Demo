ab36a78d6fcb5eb80cdc904b1435aed613a2e58f
Tanuja S <tanuja.s@robosoftin.com>
2024-01-23 10:46:54 +0530

    routed initially to login page

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+      <Route path="" element={<Login />}></Route>
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/User" element={<User />} />
         <Route path="/Signup" element={<SignUp />} />
